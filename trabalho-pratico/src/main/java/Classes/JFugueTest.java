8e7135ec569b5f362c9d9d9c46347adca59d26e9
Vinícius Medeiros <viniciussmmedeiros@gmail.com>
2022-07-03 15:00:28 -0300

    classe de testes para JFugue

---
+++ trabalho-pratico/src/main/java/Classes/JFugueTest.java
@@ -0,0 +1,9 @@
+package main.java.Classes;
+import org.jfugue.player.Player;
+
+public class JFugueTest {
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.play("C D E F G A B");
+    }
+}
