9b106b5a3cea354dfa56ef2d1a382350760cd6d0
Vinícius Medeiros <viniciussmmedeiros@gmail.com>
2022-09-27 19:11:19 -0300

    removendo comentários

--- nova-arquitetura-etapa-2/src/main/java/models/GenerateMusicResponse.java
+++ nova-arquitetura-etapa-2/src/main/java/models/GenerateMusicResponse.java
@@ -12,19 +12,15 @@ public class GenerateMusicResponse {
     }
 
     public void textProcessing() {
-        // mapeamento para conseguir os caracteres reconhecidos pelo JFugue
         KeyMapper mapping = new KeyMapper();
         Player player = new Player();
-        // player.play("T290 I30 F F F F A# A# A# A# G# G# G# G# C# C# C# C#");
-        // System.out.println("aqui ?");
 
         for (char ch : this.text.toCharArray()) {
             String jfugueKey = mapping.getMusicalAction((Character.toString(ch)));
-            // System.out.println(jfugueKey);
+
             textMusic += jfugueKey + " ";
         }
 
-        // System.out.println(textMusic);
         player.play(textMusic);
     }
 }
